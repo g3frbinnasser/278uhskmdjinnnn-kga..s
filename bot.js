609a92d815c1c8b70ab9e30bb69b12ba6c0f0e89
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-08-27 10:47:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1032,4 +1032,31 @@ client.on("message", (message) => {
         }
     }
 })
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "clear")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You Dont Have Permission');
+  if (!args[0]) return message.channel.send('You didn\'t provide any number!!!');
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(`Cleared ${args[0]} messages.`);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'log');
+
+    if (!actionlog) return message.channel.send('Cant find log channel. Are you sure that this channel exists and I have permission to view it? **CANNOT POST LOG.**');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('CLEAR')
+      .setColor(0xF16104)
+      .addField('Cleared By', `<@${message.author.id}> with ID ${message.author.id}`)
+      .addField('Cleared in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+   
+  });
+};
+
+});
 client.login(process.env.BOT_TOKEN);
