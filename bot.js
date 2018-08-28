06434fb99cde5bffb15475939f5b60d547b07f92
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-08-28 18:34:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1060,4 +1060,43 @@ client.on('message', message => {
 };
 
 });
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("382239191578312705");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("478823041741225985");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("382239191578312705");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 client.login(process.env.BOT_TOKEN);
