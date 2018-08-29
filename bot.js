aa2002470fdbc60764a98615b3927f04822d5786
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-08-29 18:53:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1174,4 +1174,26 @@ client.on("roleCreate", rc => {
   channel.sendEmbed(embed)
   }
   });
+client.on('guildMemberAdd', member => {
+  member.addRole('name', "• New")
+});
+
+
+client.on('message', message => { 
+  var prefix = "-"; //you can change it
+  let log = message.guild.channels.find('name', "log") 
+  let act = message.guild.roles.find('name', "• Verified") //Activated Rank
+  let user = message.mentions.members.first();
+  if(message.content.startsWith(prefix + "act"){
+    var embed = new Discord.RichEmbed() 
+    .setAuthor(message.author.username) 
+    .setThumbnail(user.avatarURL)
+    .addField('User Activated', `${user} get rank ${act}`)
+    .addField('By', `<@${message.author.id}>`)
+    .setTimestamp()
+    .setFooter("Codes©") //CopyRight Codes 2018©
+  log.send({embed})
+  message.channel.send({embed})
+  }
+});
 client.login(process.env.BOT_TOKEN);
