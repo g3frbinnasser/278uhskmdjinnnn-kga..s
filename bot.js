e824f287ddcbd22ac6f165711aac21608987f00b
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-08-29 15:29:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1155,32 +1155,6 @@ client.on("message", (message) => {
                 client.users.get("479469196380733470").send(yumz)
             }
 });
-client.on('message', async message => {
-            if(!message.channel.guild) return;
-             if (message.content.startsWith("-sets")) {
-let args = message.content.split(' ').slice(1).join(' ');
-            let sigMessage = await args;
-            
-            if (sigMessage === "online") {
-                client.user.setStatus("online");
-                message.author.send("Your status was set to online.");
-            }
-            if (sigMessage === "idle") {
-                client.user.setStatus("idle");
-                message.author.send("Your status was set to idle.");
-            }
-            if (sigMessage === "invisible") {
-                client.user.setStatus("invisible");
-                message.author.send("Your status was set to invisible.");
-            }
-            if (sigMessage === "dnd") {
-                client.user.setStatus("dnd");
-                message.author.send("Your status was set to dnd.");
-            }
-            // message.author.send("." + message.content);
-        
-}
-});
 client.on('message' , message => {
   var prefix = "-";
   if(message.author.bot) return;
