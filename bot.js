12bdde48cb36f4281997cd37c30c7d54bc91ce85
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-04 19:17:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1173,7 +1173,6 @@ welcomer.sendFile(canvas.toBuffer())
 })
 });
 const request = require('request');
-const fs = require('fs');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '2';
