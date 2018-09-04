1665e3d0767fcb9889ee1aeb90f0ca3e827f6982
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-04 21:35:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1145,7 +1145,14 @@ const welcomer =  member.guild.channels.find('name', 'welcome');
 var Canvas = require('canvas')
 var jimp = require('jimp')
 
-const w = [`./welcome_354.png`];
+      const w = ['./img/w1.png',
+      './img/w2.png',
+      './img/w3.png',
+      './img/w4.png',
+      './img/w5.png',
+      './img/w6.png',
+      './img/w7.png',
+      './img/w8.png'];
 
 let Image = Canvas.Image,
 canvas = new Canvas(401, 202),
@@ -1189,80 +1196,5 @@ welcomer.sendFile(canvas.toBuffer())
 
 })
 })
-});
-var jimp = require('jimp');//npm i jimp
-const prefix = '-'
-
-
-
-
-
-
-client.on('guildMemberAdd', member => {
-
-     if (member.guild.id === "478823041741225985") {
-    
-    
-var Canvas = require('canvas')
-var jimp = require('jimp')
-
-
-      const w = ['./img/w1.png',
-      './img/w2.png',
-      './img/w3.png',
-      './img/w4.png',
-      './img/w5.png',
-      './img/w6.png',
-      './img/w7.png',
-      './img/w8.png'];
-
-              let Image = Canvas.Image,
-                  canvas = new Canvas(401, 202),
-                  ctx = canvas.getContext('2d');
-              ctx.patternQuality = 'bilinear';
-              ctx.filter = 'bilinear';
-              ctx.antialias = 'subpixel';
-              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-              ctx.shadowOffsetY = 2;
-              ctx.shadowBlur = 2;
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 401, 202);
-
-      })
-
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                        if (err) return console.log(err);
-
-                              //AVATAR�
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.drawImage(ava, 152, 27, 95, 95);
-
-                                                      //wl
-                              ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '15px';
-                              ctx.fillStyle = "#FFFFFF";
-                              ctx.textAlign = "center";
-                                                         ctx.fillText(member.user.username, 200, 154);
-
-                        
-                        
-bot.channels.get("457753370954825728").sendFile(canvas.toBuffer())
-bot.channels.get("457753370954825728").send(`**__WELCOME TO SOD SERVER__**✨ ${member}`)
-
-
-
-})
-})
-
-}
 });
 client.login(process.env.BOT_TOKEN);
