e389b0fe2bce127f046336e3becb5e30cdcf9f7f
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-05 22:55:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,24 +393,6 @@ client.on('message', message => {
     return message.reply(`** No Invite Links :angry: ! **`)
     }
 });
-client.on('message', message => {
-    if (message.content === "-id") {
-    var year = message.createdAt.getFullYear()
-    var month = message.createdAt.getMonth()
-    var day = message.createdAt.getDate()
-         let embed = new Discord.RichEmbed()
-         .setAuthor(message.author.username, message.author.avatarURL)
-      .setThumbnail(message.author.avatarURL)
-        .addField("**Name:**",  '**[ ' + `${message.author.username}` + ' ]**')
-          .setThumbnail(message.author.avatarURL)
-                   .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
-      .addField('Discrim:', message.author.discriminator)
-      .addField("**The days of created the account:**", message.author.createdAt.getDate())
-        .addField("**Your Account is created in:**", message.createdAt.getFullYear())
-    
-      message.channel.send({embed});
-        }
-    });
 client.on("message", message => {
   if (message.author.bot) return;
  
