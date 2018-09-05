daa5130a4af4d1807126c57e33f28bd8fa8cf1f3
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-05 18:53:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1381,7 +1381,6 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 }
 
       });
-let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
 
   if (message.author.bot) return;
