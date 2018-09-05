0b9da430f1e3d0b8f412f95acbd1a86289526408
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-05 21:51:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1122,68 +1122,6 @@ client.on("guildMemberAdd", member => {
 انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
 });
-client.on('guildMemberAdd', member => {
-const welcomer =  member.guild.channels.find('name', 'welcome');
-
-var Canvas = require('canvas')
-var jimp = require('jimp')
-
-     const w = ['./img/w1.png',
-      './img/w2.png',
-      './img/w3.png',
-      './img/w4.png',
-      './img/w5.png',
-      './img/w6.png',
-      './img/w7.png',
-      './img/w8.png',
-      './img/w9.png',
-      './img/w10.png',
-      './img/w11.png',
-      './img/w12.png',]
-     
-let Image = Canvas.Image,
-canvas = new Canvas(401, 202),
-ctx = canvas.getContext('2d');
-ctx.patternQuality = 'bilinear';
-ctx.filter = 'bilinear';
-ctx.antialias = 'subpixel';
-ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-ctx.shadowOffsetY = 2;
-ctx.shadowBlur = 2;
-fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-if (err) return console.log(err)
-let BG = Canvas.Image;
-let ground = new Image;
-ground.src = Background;
-ctx.drawImage(ground, 0, 0, 401, 202);
-
-})
-
-let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-jimp.read(url, (err, ava) => {
-if (err) return console.log(err);
-ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-if (err) return console.log(err);
-
-
-let Avatar = Canvas.Image;
-let ava = new Avatar;
-ava.src = buf;
-ctx.drawImage(ava, 152, 27, 95, 95);
-
-
-ctx.font = '12px Arial Bold';
-ctx.fontSize = '10px';
-ctx.fillStyle = "#000000";
-ctx.textAlign = "center";
-ctx.fillText(member.user.username, 200, 154);
-welcomer.sendFile(canvas.toBuffer())
-
-
-
-})
-})
-});
 client.on('guildMemberRemove', member => {
     if (!member || !member.id || !member.guild) return;
     const guild = member.guild;
