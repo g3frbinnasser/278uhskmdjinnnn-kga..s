6a8c53ecad09528a430b12a61668655a226da998
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-06 18:33:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1361,4 +1361,30 @@ client.on('ready', function(){
    });
   });
 });
+client.on('message',message =>{
+if(message.content.startsWith('-top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://i.imgur.com/GnR2unD.png")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
 client.login(process.env.BOT_TOKEN);
