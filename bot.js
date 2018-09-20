edc548ddaf10fe9fb8df36bac421fd8916c61ba1
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-20 17:09:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -299,7 +299,7 @@ welcomer.sendFile(canvas.toBuffer())
 });
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['-help','by maric douglas'];	
+    var setGame = ['-help','-help'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
