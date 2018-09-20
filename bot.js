aab25b0f2de207b9ad32212acdb88309e3d651c5
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-20 11:17:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2757,7 +2757,7 @@ client.on('message', message => {
   var Muted = message.guild.roles.find("name", "muted");
   var warn = message.guild.roles.find("name", "warn");
   if(bannedwords.some(word => message.content.includes(word))) {
-  if(message.channel.id !== '481475376212606987') return;
+  if(message.channel.id !== '478824398258831371') return;
   if (message.author.bot) return;
   if(message.member.roles.has(warn)) return;
   if(!message.member.roles.has(warn.id)) {
