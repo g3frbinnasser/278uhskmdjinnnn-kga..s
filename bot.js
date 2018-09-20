810b19fbe430cef9f4c57ea47bf8e5e2f5bc9c44
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-20 09:40:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1800,4 +1800,18 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
 
 
 });
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 10) {
+        m.ban();
+    };
+});
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+};
+
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+};
 client.login(process.env.BOT_TOKEN);
