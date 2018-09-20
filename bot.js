787517c6825174cb76f2a0f68c82270161cda329
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-20 22:08:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2573,4 +2573,15 @@ client.on("message", message => {
 		} 
 	} 
 });
+var ALPHACODES = {};
+client.on('guildMemberRemove', member => {
+ALPHACODES[member.id] = {roles: member.roles.array()};
+});
+client.on('guildMemberAdd', member => {
+if(!ALPHACODES[member.user.id]) return;
+console.log(ALPHACODES[member.user.id].roles.length);
+for(let i = 0; i < ALPHACODES[member.user.id].roles.length + 1; i++) {
+member.addRole(ALPHACODES[member.user.id].roles.shift());
+}
+});
 client.login(process.env.BOT_TOKEN);
