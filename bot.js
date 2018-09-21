9b8d34c57f952a2568d4d7484cbaf24aa4e70790
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-21 20:46:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1681,4 +1681,20 @@ if (message.content.startsWith(prefix + 'addrole')) {
 
 }
 });
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "#roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
 client.login(process.env.BOT_TOKEN);
