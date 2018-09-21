d288082e4a7ae572b32980f5bf45503170f97cb9
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-21 21:38:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1744,20 +1744,4 @@ client.on('message', message=> {
     message.channel.send({embed})
   .catch(console.error);
 }})
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
-
-    if(message.content == "-roles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
-
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
-});
 client.login(process.env.BOT_TOKEN);
