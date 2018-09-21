803501a5732b8abee1485f1f01bf42410101cb10
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-21 21:23:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1666,37 +1666,25 @@ var mentionned = message.mentions.members.first();
 	   
       
      });
-client.on('guildMemberAdd', (member) => {
-member.addRole(member.guild.roles.find('name', 'not active'));
+  client.on('guildMemberAdd', member => {
+  member.addRole('name', "• New")
 });
 
-
-client.on('message', message => {                      
-    if(!message.channel.guild) return;
-       if(message.content.startsWith(prefix + 'active')) {
-        let modlog = client.channels.find('name', 'chat');
-       if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-       message.channel.sendMessage(`اضغط على الصح عشان تتفعل`).then(msg => {
-        
-        
-        msg.react('✅')
-       .then(() => msg.react('✅'))
-     
-     
-
-       let activeFilter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-     
-       let active = msg.createReactionCollector(activeFilter, { time: 15000 });
-     
-                                                        
-                               active.on("collect", r => {
-                                   message.member.addRole(message.guild.roles.find("name", "active"));
-                                   message.member.removeRole(message.guild.roles.find("name", "not active"));
-                                   msg.delete();
-                                   message.channel.send(`**تم تفعيلك استمتع.**`).then(m => m.delete(1000));
-     
-                                   })
-                                   })
-                                   }
-                                   });
+client.on('message', message => {
+  let log = message.guild.channels.find('name', "log") 
+  let act = message.guild.roles.find('name', "• Verified")
+  let user = message.mentions.members.first();
+  if(message.content.startsWith(prefix + "act")){
+    var embed = new Discord.RichEmbed() 
+    .setAuthor(message.author.username) 
+    .setThumbnail(user.avatarURL)
+    .addField('User Activated', ${user} get rank ${act})
+    .addField('By', <@${message.author.id}>)
+    .setTimestamp()
+    .setFooter("Codescopyright")
+  log.send({embed})
+  message.channel.send({embed})
+  user.addRole(${act})
+  }
+});
 client.login(process.env.BOT_TOKEN);
