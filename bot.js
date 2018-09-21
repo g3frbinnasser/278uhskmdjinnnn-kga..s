895e56c5e1743d67ea70e4e2f2bd443ba7dba1bb
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-21 20:54:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1666,20 +1666,51 @@ var mentionned = message.mentions.members.first();
 	   
       
      });
-var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
+const config = require('./configs.json');
 
-    if(message.content == "#roles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
 
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+const size    = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 2 * i + phase);
+  var int = Math.floor(sin * 127) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {        
+    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+        .catch(console.error);
+        
+    
+    
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
     }
+  }
+}
+
+
+
+client.on('ready', () => {
+  console.log('Bot Is Online')
+  if(config.speed <60.000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
+  setInterval(changeColor, config.speed);
 });
 client.login(process.env.BOT_TOKEN);
