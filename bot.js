47f95b0e9c78ab7e4790b91a778ca33f8c703210
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-09-21 21:35:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1672,8 +1672,6 @@ if (message.content.startsWith("#ping")) {
     message.channel.sendMessage(`Pong ! \`${Date.now() - message.createdTimestamp} ms\`:watch:`);
     }
 });
-const Discord = require('discord.js');
-
 client.on('message', message=> {
   if(message.content.startsWith('#uptime')) { 
     let ms = client.uptime; //ALPHA CODES
@@ -1746,4 +1744,20 @@ client.on('message', message=> {
     message.channel.send({embed})
   .catch(console.error);
 }})
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "-roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
 client.login(process.env.BOT_TOKEN);
