50c7f233894f7af76a2376ebd02d99bac6bb8891
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-10-26 17:54:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`welcome to Ti.`,'https://www.twitch.tv/v5bz');
+  client.user.setGame(`Welcome To The Angel.`,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
 });
 client.on('guildMemberAdd',async member => {
-const Canvas = require("canvas-prebuilt")
+const Canvas = require('canvas');
   const jimp = require('jimp');
   const w = ['./k22k.png'];
         let Image = Canvas.Image,
