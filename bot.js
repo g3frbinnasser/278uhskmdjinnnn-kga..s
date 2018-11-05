bc6c4d26620291f332c95c414de926a53c9c45c9
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-11-05 06:25:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,36 +81,46 @@ client.on("ready", () => {
         });
     });
 });
- 
- 
- 
+var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("507144416721371143")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
 client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("507144416721371143");
+    let channel = member.guild.channels.find('name', 'stranger');
     if (!channel) {
-        console.log("!the channel id it's not correct");
+        console.log("!channel fails");
         return;
     }
     if (member.id == client.user.id) {
         return;
     }
-    console.log('-');
+    console.log('made it till here!');
     var guild;
     while (!guild)
-        guild = client.guilds.get("503540196164501504");
+        guild = client.guilds.get("507144416721371143")
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
-                    setTimeout(function() {
- channel.send(`**Welcome To Śtranger Server :champagne_glass:**`) 
- channel.send(`**Invited by** ${Invite.inviter} `) ;
-                    },1500);
+                    console.log(3);
+channel.send(`**Welcome To Śtranger Server :champagne_glass:**`)
+ channel.send(`invited by ${Invite.inviter}`)
  }
             dat[Inv] = Invite.uses;
-       
-       });
-    });
+        })
+    })
 });
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","• Śtranger"));
