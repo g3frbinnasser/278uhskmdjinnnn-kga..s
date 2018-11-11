fc8dda722fa944b673a3b5ab827b084525629f4f
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-11-11 10:47:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,62 +66,6 @@ client.on('guildMemberAdd',async member => {
 });
 });
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("491686948130521112");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
-var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("491686948130521112")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'stranger');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("491686948130521112")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-channel.send(`**Welcome To Śtranger Server :champagne_glass:**`)
- channel.send(`**invited by** ${Invite.inviter}`)
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
 client.on('guildMemberAdd', (member) => {
 member.addRole(member.guild.roles.find('name', '• Śtranger'));
 });
