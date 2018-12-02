51708db8c114a74f88188112abe74ad0a67ae704
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-12-02 18:55:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,24 +8,34 @@ if(mohamed) {
 setTimeout(() => mohamed.send(`**.. Welcome To, - Future Community. :leaves::tulip: ** `), 4000)        
 }
 });
-client.on('ready',async () => {
-  let GUILDID = '517345819406368769'; // اي دي السيرفر
-  let CHANNELID = '517597766583713802'; // اي دي الروم
-  voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
- 
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
- 
-    if(channel.type === 'ji.') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+   client.on('message', message => {
+const yt = require('ytdl-core');
+  if (message.content.startsWith('ji.')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
     }
+    voiceChannel.join()
+      .then(connnection => {
+        let stream = yt('https://www.youtube.com/watch?v=Ktync4j_nmA', {audioonly: true});
+        const dispatcher = connnection.playStream(stream);
+        dispatcher.on('end', () => {
+          voiceChannel.leave();
+        });
+      });
   }
+  
+  if (message.content.startsWith('lv.')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
+    }
+voiceChannel.leave();
+  }
+
 });
 client.login(process.env.BOT_TOKEN)
