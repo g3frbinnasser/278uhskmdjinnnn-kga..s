c4746d7333dee1732a6ae1946b7fc341c251546a
g3frbinnasser <42671461+g3frbinnasser@users.noreply.github.com>
2018-12-07 15:11:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -444,26 +444,26 @@ var prefix = "#";
  message.delete(); 
 };     
 });
-const adminprefix = "K"; 
-const devs = ['427476285166452748'];
+const adminprefix = "#"; 
+const devs = ['507920205973028879'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
    
-if (message.content.startsWith(adminprefix + 'sg')) {
+if (message.content.startsWith(adminprefix + 'playing')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} :white_check_mark:  **`)
 } else
-  if (message.content.startsWith(adminprefix + 'sn')) {
+  if (message.content.startsWith(adminprefix + 'name')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغير اسمك :white_check_mark: `)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'sa')) {
+  if (message.content.startsWith(adminprefix + 'avatar')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغيير صورتك :white_check_mark: `);
       } else    
-if (message.content.startsWith(adminprefix + 'st')) {
+if (message.content.startsWith(adminprefix + 'streaming')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
     message.channel.sendMessage(`**:white_check_mark: تم تغير الحاله الى واتشنق ${argresult}**`)
 }
